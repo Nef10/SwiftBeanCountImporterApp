725a1b557130dba695b743db55a771768b0de565
Steffen Kötte <steffen.koette@gmail.com>
2017-08-28 19:36:40 -0700

    Initial Commit

---
+++ BeanCountImporter/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BeanCountImporter
+//
+//  Created by Steffen Kötte on 2017-08-28.
+//  Copyright © 2017 Steffen Kötte. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}
