e02e26e902f40fd1ef378106b95663f05cbedeb5
Steffen Kötte <steffen.koette@gmail.com>
2018-05-03 22:50:06 -0700

    Save Payee, Description and Account to user defaults if requested

---
+++ BeanCountImporter/Model/ImportedTransaction.swift
@@ -0,0 +1,17 @@
+//
+//  ImportedTransaction.swift
+//  BeanCountImporter
+//
+//  Created by Steffen Kötte on 2018-04-29.
+//  Copyright © 2018 Steffen Kötte. All rights reserved.
+//
+
+import Cocoa
+import SwiftBeanCountModel
+
+struct ImportedTransaction {
+
+    let transaction: Transaction
+    let originalDescription: String
+
+}
